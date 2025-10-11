26fa4182985120fe9842cad8ac1d6d8aefb25ba6
Leiner Barrios <leinerbarrios99.ljbm@gmail.com>
2025-10-10 21:27:42 -0500

    feat(login): update login page layout and add MedySupply logo

--- frontend/src/web/app.routes.ts
+++ frontend/src/web/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'login',
     loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginPage),
   },
   {
     path: 'style-demo',
     loadComponent: () => import('./pages/style-demo/style-demo.page').then((m) => m.StyleDemoPage),
   },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  }
 ];
--- frontend/src/web/pages/login/login.page.ts
+++ frontend/src/web/pages/login/login.page.ts
@@ -1,14 +1,13 @@
+import { IonContent, IonImg } from '@ionic/angular/standalone';
+
 import { Component } from '@angular/core';
-import { IonText, IonContent, IonButton } from '@ionic/angular/standalone';
 
-import { RouterLink } from '@angular/router';
-
-import { ExploreContainerComponent } from '@shared/components/explore-container/explore-container.component';
 import { AppAuthenticatorComponent } from '@shared/auth/organisms/app-authenticator/app-authenticator.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: 'login.page.html',
-  imports: [IonContent, IonText, IonButton, RouterLink, ExploreContainerComponent, AppAuthenticatorComponent],
+  styleUrls: ['login.page.scss'],
+  imports: [IonImg, IonContent, AppAuthenticatorComponent],
 })
 export class LoginPage {}
