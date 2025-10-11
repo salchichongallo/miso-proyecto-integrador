ba99efec835df8b81091e33f73c3d829ddf99710
Leiner Barrios <leinerbarrios99.ljbm@gmail.com>
2025-10-10 22:40:00 -0500

    feat(main-layout): create main layout component with routing and dynamic page titles

--- frontend/src/web/app.routes.ts
+++ frontend/src/web/app.routes.ts
@@ -6,12 +6,23 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginPage),
   },
   {
-    path: 'style-demo',
-    loadComponent: () => import('./pages/style-demo/style-demo.page').then((m) => m.StyleDemoPage),
-  },
-  {
     path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
+    loadComponent: () => import('./layouts/main-layout/main-layout.component').then((m) => m.MainLayoutComponent),
+    children: [
+      {
+        path: 'seller-registration',
+        loadComponent: () =>
+          import('./pages/seller-registration/seller-registration.page').then((m) => m.SellerRegistrationPage),
+      },
+      {
+        path: 'style-demo',
+        loadComponent: () => import('./pages/style-demo/style-demo.page').then((m) => m.StyleDemoPage),
+      },
+      {
+        path: '',
+        redirectTo: 'seller-registration',
+        pathMatch: 'full',
+      }
+    ]
   }
 ];
