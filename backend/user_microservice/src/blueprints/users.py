1dcbac0daa3bc6b7d9e469972882cc5fb2d4ffba
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-10-12 10:46:50 -0500

    refactor(users): remove route prefix

--- backend/user_microservice/src/blueprints/users.py
+++ backend/user_microservice/src/blueprints/users.py
@@ -5,7 +5,6 @@ from ..commands.ping import Ping
 users_blueprint = Blueprint("users", __name__)
 
 
-@users_blueprint.get("/users/ping")
+@users_blueprint.get("/ping")
 def ping():
     return jsonify(Ping().execute())
-
