dc5ed572391fe4bc8f0d433889ce16ce9f035621
Leiner Barrios <leinerbarrios99.ljbm@gmail.com>
2025-10-17 10:02:23 -0500

    feat(seller-registration): add RegisterSellerRequest and RegisterSellerResponse interfaces, and implement SellerService for seller registration

---
+++ frontend/src/web/pages/seller-registration/services/seller/seller.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { RegisterSellerRequest } from '@web/pages/seller-registration/interfaces/register-seller-request.interface';
+import { RegisterSellerResponse } from '@web/pages/seller-registration/interfaces/reguster-seller-response.interface';
+
+import { environment } from '@env/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SellerService {
+  private readonly baseUrl = environment.vendorMicroserviceUrl;
+  private readonly http = inject(HttpClient);
+
+  public registerSeller(sellerData: RegisterSellerRequest): Observable<RegisterSellerResponse> {
+    return this.http.post<RegisterSellerResponse>(this.baseUrl, sellerData);
+  }
+}
