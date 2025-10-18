52d6f62d13e7b389b9c46b3d5833d322741b2754
Leiner Barrios <leinerbarrios99.ljbm@gmail.com>
2025-10-18 16:36:23 -0500

    feat(supplier-service): implement SupplierService and associated tests for supplier registration

---
+++ frontend/src/web/services/supplier/supplier.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { environment } from '@env/environment';
+
+import { RegisterSupplierResponse } from '@web/pages/supplier-registration/interfaces/register-supplier-response.interface';
+import { RegisterSupplierRequest } from '@web/pages/supplier-registration/interfaces/register-supplier-request.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SupplierService {
+  private readonly baseUrl = environment.providerMicroserviceUrl;
+  private readonly http = inject(HttpClient);
+
+  public createSupplier(body: RegisterSupplierRequest): Observable<RegisterSupplierResponse> {
+    return this.http.post<RegisterSupplierResponse>(this.baseUrl, body);
+  }
+}
