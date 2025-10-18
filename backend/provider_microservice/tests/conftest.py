88c079aef6055a99148037bd0fab4f1059fae4cb
JhormanGalindo <j.galindop@uniandes.edu.co>
2025-10-18 10:50:59 -0500

    chore: add coverage configuration files for integration and unit tests

--- backend/provider_microservice/tests/conftest.py
+++ backend/provider_microservice/tests/conftest.py
@@ -1,7 +1,6 @@
 import os
 import sys
-import pytest
-from unittest.mock import patch
+
 
 # --- Configuración de paths ---
 CURRENT_DIR = os.path.dirname(__file__)
@@ -9,21 +8,3 @@ PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, ".."))
 SRC_PATH = os.path.join(PROJECT_ROOT, "src")
 sys.path.insert(0, PROJECT_ROOT)
 sys.path.insert(0, SRC_PATH)
-
-# --- 💡 APLICAR MOCKS DE COGNITO ANTES DE IMPORTAR APP ---
-with patch("flask_cognito.cognito_auth_required", lambda f: f), \
-     patch("flask_cognito.cognito_group_permissions", lambda x: (lambda f: f)), \
-     patch("flask_cognito.current_cognito_jwt", {
-         "username": "test-user",
-         "email": "test@example.com",
-         "cognito:groups": ["admins", "vendors"]
-     }):
-    from src.main import app
-
-
-# --- Fixture de cliente Flask ---
-@pytest.fixture
-def client():
-    app.testing = True
-    with app.test_client() as client:
-        yield client
