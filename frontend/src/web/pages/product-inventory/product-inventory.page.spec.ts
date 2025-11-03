37eb386cf1fa5ce79a8922183402fc7677f46875
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-11-02 19:23:19 -0500

    feat(products): fetch product inventory

--- frontend/src/web/pages/product-inventory/product-inventory.page.spec.ts
+++ frontend/src/web/pages/product-inventory/product-inventory.page.spec.ts
@@ -1,17 +1,54 @@
 import { TestBed } from '@angular/core/testing';
+
+import { ProductService } from '@web/services/product/product.service';
+
 import { ProductInventoryPage } from './product-inventory.page';
+
+jest.mock('@web/services/product/product.service');
 
 describe('ProductInventoryPage', () => {
   let component: ProductInventoryPage;
+  let productService: jest.Mocked<ProductService>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ProductInventoryPage],
+      providers: [ProductInventoryPage, ProductService],
     });
     component = TestBed.inject(ProductInventoryPage);
+    productService = TestBed.inject(ProductService) as jest.Mocked<ProductService>;
+    productService.search.mockResolvedValue([]);
   });
 
-  it('should be defined', () => {
-    expect(component).toBeDefined();
+  it('should load initial products on init', async () => {
+    await component.ngOnInit();
+
+    expect(productService.search).toHaveBeenCalledWith({
+      productName: '',
+      batch: '',
+      status: '',
+      warehouseName: '',
+    });
+    expect(component['products']()).toEqual([]);
+  });
+
+  it('should handle errors when loading initial products', async () => {
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+    const toastCreateSpy = jest.spyOn(component['toast'], 'create').mockResolvedValue({
+      present: jest.fn().mockResolvedValue(undefined),
+    } as any);
+
+    const error = new Error('Test error');
+    productService.search.mockRejectedValueOnce(error);
+    await component.ngOnInit();
+
+    expect(consoleErrorSpy).toHaveBeenCalledWith(error);
+    expect(toastCreateSpy).toHaveBeenCalledWith({
+      message: `Error al consultar productos. ${error.message}`,
+      duration: 7000,
+      color: 'danger',
+    });
+
+    consoleErrorSpy.mockRestore();
+    toastCreateSpy.mockRestore();
   });
 });
--- frontend/src/web/pages/product-inventory/product-inventory.page.ts
+++ frontend/src/web/pages/product-inventory/product-inventory.page.ts
@@ -1,7 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  LoadingController,
+  IonCardContent,
+  IonCardSubtitle,
+  IonText,
+  ToastController,
+} from '@ionic/angular/standalone';
+
+import { Product } from '@mobile/models/product.model';
+import { ProductService } from '@web/services/product/product.service';
+import { ProductInventaryItem } from '@web/components/product-inventary-item/product-inventary-item';
 
 @Component({
   selector: 'app-product-inventory',
   templateUrl: 'product-inventory.page.html',
+  imports: [IonText, IonCardSubtitle, IonCardContent, IonCardTitle, IonCardHeader, IonCard, ProductInventaryItem],
+  styleUrl: './product-inventory.page.scss',
 })
-export class ProductInventoryPage {}
+export class ProductInventoryPage implements OnInit {
+  private readonly productsService = inject(ProductService);
+
+  protected readonly products = signal<Product[]>([]);
+
+  private readonly loader = inject(LoadingController);
+
+  private readonly toast = inject(ToastController);
+
+  async ngOnInit() {
+    await this.loadInitialProducts();
+  }
+
+  private async loadInitialProducts() {
+    const loading = await this.showLoader();
+    try {
+      await this.loadProducts();
+    } catch (error: any) {
+      console.error(error);
+      await this.showToastError(error);
+    } finally {
+      await loading.dismiss();
+    }
+  }
+
+  private async loadProducts() {
+    // TODO: get search from form
+    const products = await this.productsService.search({ productName: '', batch: '', status: '', warehouseName: '' });
+    this.products.set(products);
+  }
+
+  private async showLoader() {
+    const loading = await this.loader.create({
+      message: 'Cargando...',
+      keyboardClose: false,
+      backdropDismiss: false,
+    });
+    await loading.present();
+    return loading;
+  }
+
+  private async showToastError(error: any) {
+    await this.loader.dismiss();
+    return this.toast
+      .create({ message: `Error al consultar productos. ${error?.message}`, duration: 7000, color: 'danger' })
+      .then((toast) => toast.present());
+  }
+}
