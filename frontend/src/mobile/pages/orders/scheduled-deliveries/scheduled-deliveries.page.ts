aa8b352049d97af3ef26da67ade28b4df1dc7279
Jaime Gallo <j.gallom@uniandes.edu.co>
2025-11-08 16:55:17 -0500

    feat(orders): layout schedule delivery card

--- frontend/src/mobile/pages/orders/scheduled-deliveries/scheduled-deliveries.page.ts
+++ frontend/src/mobile/pages/orders/scheduled-deliveries/scheduled-deliveries.page.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonTitle, IonToolbar, IonContent, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { ScheduledDeliveryCardComponent } from '@mobile/components/schedule-delivery-card/scheduled-delivery-card';
 
 @Component({
   selector: 'app-scheduled-deliveries',
   templateUrl: './scheduled-deliveries.page.html',
   styleUrls: ['./scheduled-deliveries.page.scss'],
-  imports: [IonBackButton, IonButtons, IonContent, IonToolbar, IonHeader, IonTitle],
+  imports: [IonBackButton, IonButtons, IonContent, IonToolbar, IonHeader, IonTitle, ScheduledDeliveryCardComponent],
 })
 export class ScheduledDeliveriesPage implements OnInit {
   ngOnInit() {
